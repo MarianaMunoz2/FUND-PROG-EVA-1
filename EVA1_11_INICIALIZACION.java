9b6ee810190503a5e503889af604ac34e318820f
MarianaMunoz2 <143203307+MarianaMunoz2@users.noreply.github.com>
2023-09-24 13:56:56 -0600

    Add files via upload

---
+++ EVA1_11_INICIALIZACION.java
@@ -0,0 +1,26 @@
+package com.mycompany.eva1_11_inicializacion;
+
+
+public class EVA1_11_INICIALIZACION {
+
+    public static void main(String[] args) {
+        //los parentesìs denotan una funciòn:
+        //printl()=funciòn
+        //main()=funciòn
+        System.out.println("Hola mundo")
+        int valor = 0;//inicializar la variable para usarla
+        System.out.println(valor);
+        
+        //declaramos e inicializamos 3 variables double
+        double salario = 0, iva = 0.16, subtotal = 10000;
+        
+        final int califa_pasar = 70;
+        final String ESCUELA = "Instituto Tecnològico de Chihuahua II"
+        
+        //error: solo se puede asignar una sola vez
+        //califa_pasar = 100;
+        //califa_pasar = 20;
+        
+        
+    }
+}
