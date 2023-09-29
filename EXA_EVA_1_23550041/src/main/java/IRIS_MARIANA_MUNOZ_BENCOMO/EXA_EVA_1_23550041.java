5a9bfb2d05081cb0163bd6f211eff1dd82f26ed2
MarianaMunoz2 <143203307+MarianaMunoz2@users.noreply.github.com>
2023-09-29 12:13:40 -0600

    Add files via upload

---
+++ EXA_EVA_1_23550041/src/main/java/IRIS_MARIANA_MUNOZ_BENCOMO/EXA_EVA_1_23550041.java
@@ -0,0 +1,69 @@
+package IRIS_MARIANA_MUNOZ_BENCOMO;
+
+import java.util.Scanner;
+ 
+public class EXA_EVA_1_23550041 {
+    //VALORES
+    String equipo;
+    String proyecto;
+    String integ0;
+    String integ1;
+    String integ2;
+    String integ3;
+    String integ4;
+    String integ5;
+    String integ6;
+    String integ7;
+    String integ8;
+    String integ9;
+    
+    public static void main(String[] args) {
+        //clase Scanner - Para usar el teclado.
+        
+        Scanner scanner=new Scanner(System.in);
+        
+        System.out.println("Hola usuario, ingresa el nombre de tu equipo");
+        String equipo=scanner.nextLine();
+        System.out.println("Ingresa el nombre de tu proyecto");
+        String proyecto=scanner.nextLine();
+        System.out.println("Ingresa el nombre y número de control del primer integrante de tu equipo");
+        String integ0=scanner.nextLine();
+        System.out.println("Ingresa el nombre y número de control del segundo integrante de tu equipo");
+        String integ1=scanner.nextLine();
+        System.out.println("Ingresa el nombre y número de control del tercer integrante de tu equipo");
+        String integ2=scanner.nextLine();
+        System.out.println("Ingresa el nombre y número de control del cuarto integrante de tu equipo");
+        String integ3=scanner.nextLine();
+        System.out.println("Ingresa el nombre y número de control del quinto integrante de tu equipo");
+        String integ4=scanner.nextLine();
+        System.out.println("Ingresa el nombre y número de control del sexto integrante de tu equipo");
+        String integ5=scanner.nextLine();
+        System.out.println("Ingresa el nombre y número de control del séptimo integrante de tu equipo");
+        String integ6=scanner.nextLine();
+        System.out.println("Ingresa el nombre y número de control del octavo integrante de tu equipo");
+        String integ7=scanner.nextLine();
+        System.out.println("Ingresa el nombre y número de control del noveno integrante de tu equipo");
+        String integ8=scanner.nextLine();
+        System.out.println("Ingresa el nombre y número de control del décimo integrante de tu equipo");
+        String integ9=scanner.nextLine();
+        
+        System.out.println("Los datos que ingresaste son los siguientes:");
+        System.out.println("Nombre del equipo:" );
+        System.out.println(equipo);
+        System.out.println("Nombre del proyecto:" );
+        System.out.println(proyecto);
+        System.out.println("Integrantes:" );
+        System.out.println(integ0);
+        System.out.println(integ1);
+        System.out.println(integ2);
+        System.out.println(integ3);
+        System.out.println(integ4);
+        System.out.println(integ5);
+        System.out.println(integ6);
+        System.out.println(integ7);
+        System.out.println(integ8);
+        System.out.println(integ9);
+        System.out.println("GRACIAS POR JUGAR :) ");
+        
+    }
+}
---
+++ EXA_EVA_1_23550041/src/main/java/IRIS_MARIANA_MUNOZ_BENCOMO/EXA_TEO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package IRIS_MARIANA_MUNOZ_BENCOMO;
+
+/**
+ *
+ * @author irism
+ */
+public class EXA_TEO {
+    
+}
